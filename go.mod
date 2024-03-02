31e82f44031b81ed90667255386630d5b24bbbce
paste <147841049+pastc@users.noreply.github.com>
2024-03-02 13:42:25 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module provago
+
+go 1.22.0
---
+++ provago_test.go
@@ -0,0 +1,11 @@
+package provago
+
+import "testing"
+
+func TestGenerate(t *testing.T) {
+	generate(1000000)
+}
+
+func TestRead(t *testing.T) {
+	read(100)
+}
