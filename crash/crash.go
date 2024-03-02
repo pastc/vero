8fa34c8e037853ba9ae6b5e518a80b4837bb1d32
paste <147841049+pastc@users.noreply.github.com>
2024-03-02 20:27:33 +0900

    Removed unnecessary toFixed

--- crash/crash.go
+++ crash/crash.go
@@ -12,6 +12,7 @@ import (
 func Crash(seed string, houseEdgePercent float64) float64 {
 	hash := hex.EncodeToString(hmac.New(sha256.New, []byte(seed)).Sum(nil))
 
+	// Use the most significant 52-bit from the hash to calculate the crash point
 	h, err := strconv.ParseInt(hash[0:52/4], 16, 64)
 	if err != nil {
 		log.Fatal(err)
@@ -19,22 +20,9 @@ func Crash(seed string, houseEdgePercent float64) float64 {
 	e := math.Pow(2, 52)
 	result := (100*e - float64(h)) / (e - float64(h))
 
+	// houseEdgePercent of 5 will result in modifier of 0.95 = 5% house edge with a lowest crashpoint of 100
 	houseEdgeModifier := 1 - houseEdgePercent/100
 	endResult := math.Max(100, result*houseEdgeModifier)
 
 	return math.Floor(endResult)
-
-	//	const hash = crypto.createHmac('sha256', seed).digest('hex');
-	//	console.log(`Hash: ${hash}`);
-	//	// Use the most significant 52-bit from the hash to calculate the crash point
-	//	const h = Number.parseInt(hash.slice(0, 52 / 4), 16);
-	//	const e = Math.pow(2, 52);
-	//	const result = (100 * e - h) / (e - h);
-	//
-	//	// houseEdgePercent of 5 will result in modifier of 0.95 = 5% house edge with a lowest crashpoint of 100
-	//	const houseEdgeModifier = 1 - houseEdgePercent / 100;
-	//	const endResult = Math.max(100, result * houseEdgeModifier);
-	//
-	//	return Math.floor(endResult);
-	//
 }
--- crash/crash_test.go
+++ crash/crash_test.go
@@ -2,20 +2,10 @@ package crash
 
 import (
 	"fmt"
-	"math"
 	"testing"
 )
 
 func TestCrash(t *testing.T) {
-	crash := Crash("2826d440b0fcad643e3008693c3a93ef81b31675ca00d686e44c40d5e83d7bb6", 6.66)
-	fmt.Println(toFixed(crash/100, 2))
-}
-
-func round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
-
-func toFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
+	crash := Crash("cadaaef371bc977aae209dc9be1a30665550adf89fa40fc17771051914d1f9fc", 6.66)
+	fmt.Println(crash / 100)
 }
