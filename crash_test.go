306386927203cef0024288521104485ea4fcff7e
paste <147841049+pastc@users.noreply.github.com>
2024-03-06 10:52:38 +0900

    Fixed dumb mistake

--- crash_test.go
+++ crash_test.go
@@ -38,8 +38,8 @@ func TestCrash(t *testing.T) {
 }
 
 func FuzzCrash(f *testing.F) {
+	f.Add(0)
 	f.Fuzz(func(t *testing.T, seed int) {
-		f.Add(0)
 		_, err := Crash(internal.Hash256(strconv.Itoa(seed)))
 		if err != nil {
 			t.Fatalf("got %v", err)
