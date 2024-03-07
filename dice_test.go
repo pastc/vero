cfa249de5d3c5144dbedca48d8bd61fdebed8f7c
paste <147841049+pastc@users.noreply.github.com>
2024-03-07 17:21:22 +0900

    Dice returns float instead of int

--- dice_test.go
+++ dice_test.go
@@ -13,18 +13,18 @@ func TestDice(t *testing.T) {
 		nonce                  int
 		iteration              int
 		want                   struct {
-			value float64
+			value int
 		}
 	}{
 		{"964cd1665174434d3b82b0a7e9dd5b8bbbc58056a4c3d411d89afcdc2141fa81",
 			"20244eaefdc61956bde0c835b0698d8bf5eeddaa7a27e9fae53db5da5a3a0967", 32139, 0, struct {
-				value float64
-			}{37.50},
+				value int
+			}{3750},
 		},
 		{"ad0111b329b54e2947d1ee14c7b40242019bae11114775932b7865c227636a3a",
 			"2cf0c2cb4476cdf70ce68a42bc86b17814dafead8a8c30c128d203dc7270816f", 43289, 0, struct {
-				value float64
-			}{35.61},
+				value int
+			}{3561},
 		},
 	}
 
@@ -35,7 +35,7 @@ func TestDice(t *testing.T) {
 				t.Fatalf("got %v", err)
 			}
 			if value != tt.want.value {
-				t.Errorf("got %f, want %f", value, tt.want.value)
+				t.Errorf("got %d, want %d", value, tt.want.value)
 			}
 		})
 	}
